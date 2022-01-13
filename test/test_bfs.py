d5c19191ac4b934b35e8ebeaa734f723d8d030c8
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-12 20:27:35 -0800

    Test all nodes being visited during traversal

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,6 +1,8 @@
 # write tests for bfs
 import pytest
 from search import graph
+import os
+currentdir = os.getcwd() #GitHub/project2/
 
 @pytest.fixture
 def test_bfs_traversal():
@@ -11,6 +13,17 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
+
+    tiny_network = graph.Graph(os.path.join(currentdir,"data/tiny_network.adjlist"))
+
+    all_nodes = list(tiny_network.graph.nodes) #list of every node in tiny_network.adjlist
+    traversal_results = tiny_network.bfs(start =all_nodes[0],end = None ) #will return order of traversal when end = None
+    assert all_nodes.sort() == traversal_results.sort() #if bfs traversed graph correctly, it should have visited every node. 
+    #If that happened, then the sorted traversal_results list and sorted all_nodes list should be identical. 
+    #This implicitly also tests the correct number of nodes were returned. Test that is the case
+
+
+    #The test above does not check the nodes were returned in the correct order
     pass
 
 def test_bfs():
