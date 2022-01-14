d3a7f1574859936ba3438cc3c7dea7ace52a127c
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-14 10:48:08 -0800

    see what currentdir is

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -61,8 +61,7 @@ def test_bfs_traversal():
     assert test_graph.bfs(start = "C", end = None) == ["C","F","G","K"]
     assert test_graph.bfs(start = "B", end = None) == ["B","E","D","H"]
 
-    print(currentdir)
-    print('hello')
+    print(currentdir == 'here')
 
     ############# Test 2 #############
     tiny_network = Graph(filename = "./data/tiny_network.adjlist")
