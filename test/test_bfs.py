d90e3dd0498db92843281afaeb61ce32d4bdb363
shirondru <59421145+shirondru@users.noreply.github.com>
2022-01-13 20:35:23 -0800

    Add test that bfs finds shortest path

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,7 +1,8 @@
 # write tests for bfs
 import pytest
-from search import Graph
+from search import Graph 
 import os
+import random
 currentdir = os.getcwd() #GitHub/project2/
 
 # @pytest.fixture
@@ -37,4 +38,22 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
+    tiny_network = Graph(os.path.join(currentdir,"data/tiny_network.adjlist"))
+    length_3_paths = [] #this will contain length 3 paths that are the only possible length 3 paths between the starting node and end node. Therefore they are also the shortest paths. I will test that BFS can find this path
+    max_tests = 100
+    test_num = 0
+    for starting_node in list(tiny_network.graph.nodes): #iterate through every node in the network or until max_tests condition is reached, whichever comes first
+        if len(list(tiny_network.graph[starting_node])) == 1: #Go on to look for neighbors of starting nodes that only have one neighbor
+            next_node = list(tiny_network.graph[starting_node])[0] #Grab the single neighbor of the starting node
+            for third_node in list(tiny_network.graph[next_node]): #Because the starting node only had one outgoing neighbor, all outgoing neighbors of the next node (except for the starting node) will create a unique shortest path; the only possible path is the shortest (length 3) path. If BFS worked, it should find it
+                if third_node != starting_node: #If third node happens to be the starting node, BFS would find a length 1 path from starting node -> starting node. Ignore these because that is too easy of a test
+                    length_3_paths.append([starting_node,next_node,third_node]) #add the unique shortest path to the list
+                    test_num += 1
+            if test_num >= max_tests: #break so the tests don't take too long
+                break
+
+    for length_3_path in length_3_paths:
+        starting_node = length_3_path[0]
+        third_node = length_3_path[-1]
+        assert tiny_network.bfs(starting_node,third_node) == length_3_path
     pass
